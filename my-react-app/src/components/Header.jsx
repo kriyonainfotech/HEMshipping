98f431924f01f7d8e90ba954ba900d5b8ca2cf7d
unknown <kriyonainfotech@gmail.com>
2025-03-13 18:15:18 +0530

    save

--- my-react-app/src/components/Header.jsx
+++ my-react-app/src/components/Header.jsx
@@ -16,22 +16,19 @@ const Header = () => {
             <div className="md:flex space-x-6">
               <div className="flex flex-wrap items-center space-x-2 cursor-pointer hover:text-gray-200 transition-colors duration-200 poppins">
                 <IoCall className="text-lg" />
-                <span className="text-sm hidden sm:inline">
-                  +1 (123) 456-7890
-                </span>
+                <span className="text-sm hidden sm:inline">+917435918212</span>
               </div>
               <div className="flex flex-wrap items-center space-x-2 cursor-pointer hover:text-gray-200 transition-colors duration-200 poppins">
                 <IoMdMailUnread className="text-lg " />
-                <span className="text-sm hidden sm:inline">
-                  info@shippinglogistics.com
-                </span>
+                <span className="text-sm hidden sm:inline">+919825897999</span>
               </div>
             </div>
 
             <div className="flex flex-wrap items-center space-x-2 cursor-pointer hover:text-gray-200 transition-colors duration-200 poppins">
               <FaLocationDot />
               <span className="text-sm hidden sm:inline">
-                1234 Sed ut perspiciatis Road, At vero eos, D58 8975, London.
+                201-202/212, Varunisha Crossway, Plot # 589, Ward 12-C,
+                Gandhidham - Kachchh - 370201.
               </span>
             </div>
           </div>
@@ -41,7 +38,10 @@ const Header = () => {
         <div className="container mx-auto px-12 py-4 flex justify-between items-center">
           {/* Logo */}
           <div className="flex items-center">
-            <Link to={'/'}> <img src="/logo2.JPG" alt="Shipping Logo" className="h-22" /></Link>
+            <Link to={"/"}>
+              {" "}
+              <img src="/logo2.JPG" alt="Shipping Logo" className="h-22" />
+            </Link>
           </div>
 
           {/* Desktop Navigation */}
@@ -102,21 +102,22 @@ const Header = () => {
 
         {/* Mobile Menu */}
         <div
-          className={`lg:hidden bg-gray-100 border-t border-gray-200 overflow-hidden transition-all duration-300 ease-in-out ${isMenuOpen ? "max-h-96 opacity-100" : "max-h-0 opacity-0"
-            }`}
+          className={`lg:hidden bg-gray-100 border-t border-gray-200 overflow-hidden transition-all duration-300 ease-in-out ${
+            isMenuOpen ? "max-h-96 opacity-100" : "max-h-0 opacity-0"
+          }`}
         >
           <div className="w-full flex justify-center text-center">
             <div className="px-8 py-4 flex flex-col space-y-4">
-              <Link to={'/'} className="text-pri font-semibold border-b">
+              <Link to={"/"} className="text-pri font-semibold border-b">
                 Home
               </Link>
-              <Link to={'/services'} className="text-gray-700 hover-text-pri">
+              <Link to={"/services"} className="text-gray-700 hover-text-pri">
                 Services
               </Link>
-              <Link to={'/about'} className="text-gray-700 hover-text-pri">
+              <Link to={"/about"} className="text-gray-700 hover-text-pri">
                 About Us
               </Link>
-              <Link to={'/contact'} className="text-gray-700 hover-text-pri">
+              <Link to={"/contact"} className="text-gray-700 hover-text-pri">
                 Contact
               </Link>
               <button className="bg-sec text-white px-6 py-2 rounded-md hover:bg-blue-700 transition duration-300 mt-2">
